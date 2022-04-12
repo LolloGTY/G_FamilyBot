dc4299841ce8a72603e6f5d9ddb010223b5e3eaf
LolloGTY <101922317+LolloGTY@users.noreply.github.com>
2022-04-12 15:25:15 +0200

    xx

--- index.js
+++ index.js
@@ -205,3 +205,22 @@ client.on("messageCreate", message => {
 })
 
 
+client.on("messageCreate", message => {
+    if (message.content.startsWith("!say")) {
+        var args = message.content.split(/\s+/);
+        var testo;
+        testo = args.slice(1).join(" ");
+        if (!testo) {
+            return message.channel.send("Inserire un messaggio");
+        }
+        if (message.content.includes("@everyone") || message.content.includes("@here")) {
+            return message.channel.send("Non taggare everyone o here");
+        }
+        message.delete()
+
+        //Messaggio classico
+        message.channel.send(testo)
+
+    }
+})
+
