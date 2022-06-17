9a30842e845c06f3e6e1dc0f16397f1e97965b2c
LolloGTY <101922317+LolloGTY@users.noreply.github.com>
2022-06-17 20:13:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client({
+global.client = new Discord.Client({
     intents:["GUILDS","GUILD_MEMBERS","GUILD_MESSAGES","GUILD_INTEGRATIONS","GUILD_MESSAGE_REACTIONS", "GUILD_VOICE_STATES","DIRECT_MESSAGES"],
 	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
 });
