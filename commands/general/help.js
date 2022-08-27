778fc30233702bf14a67c821fee81fdfcc5fc29d
LolloGTY <101922317+LolloGTY@users.noreply.github.com>
2022-08-27 09:25:19 +0200

    Create help.js

---
+++ commands/general/help.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: "help",
+    description: "jdsfkjd",
+    execute(message){
+        message.channel.send("Help coming soon")
+    }
+}
